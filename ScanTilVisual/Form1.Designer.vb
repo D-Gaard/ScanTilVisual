b148d129ee617ede86884514cf0ebe2ceb7a39e2
Carsten Daugaard <cdg@2bm.dk>
2019-04-03 10:17:08 +0200

    Tegn array af pictureboxes

--- ScanTilVisual/Form1.Designer.vb
+++ ScanTilVisual/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FormScanTilVisual
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,61 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.ButtonTegn = New System.Windows.Forms.Button()
+        Me.TextBoxInput = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.PictureBoxScaning = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBoxScaning, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonTegn
+        '
+        Me.ButtonTegn.Location = New System.Drawing.Point(12, 38)
+        Me.ButtonTegn.Name = "ButtonTegn"
+        Me.ButtonTegn.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonTegn.TabIndex = 0
+        Me.ButtonTegn.Text = "ButtonTegn"
+        Me.ButtonTegn.UseVisualStyleBackColor = True
+        '
+        'TextBoxInput
+        '
+        Me.TextBoxInput.Location = New System.Drawing.Point(12, 67)
+        Me.TextBoxInput.Name = "TextBoxInput"
+        Me.TextBoxInput.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxInput.TabIndex = 1
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'PictureBoxScaning
+        '
+        Me.PictureBoxScaning.Location = New System.Drawing.Point(688, 12)
+        Me.PictureBoxScaning.Name = "PictureBoxScaning"
+        Me.PictureBoxScaning.Size = New System.Drawing.Size(100, 75)
+        Me.PictureBoxScaning.TabIndex = 3
+        Me.PictureBoxScaning.TabStop = False
+        '
+        'FormScanTilVisual
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.PictureBoxScaning)
+        Me.Controls.Add(Me.TextBoxInput)
+        Me.Controls.Add(Me.ButtonTegn)
+        Me.Name = "FormScanTilVisual"
+        Me.Text = "ScanToVisual"
+        CType(Me.PictureBoxScaning, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ButtonTegn As Button
+    Friend WithEvents TextBoxInput As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents PictureBoxScaning As PictureBox
 End Class
--- ScanTilVisual/Form1.vb
+++ ScanTilVisual/Form1.vb
@@ -1,3 +1,54 @@
-﻿Public Class Form1
+﻿Public Class FormScanTilVisual
+    '********************************
+    'ScanTilVisual - Skal anvede en tekstfil oprettet af en arduino til
+    '
+    'Krav:
+    '
+    'Af: Mads Daugaard - April 2019
+    '*********************************
 
+    Const xStart As Integer = 100
+    Const yStart As Integer = 100
+
+    Const aScanx As Integer = 10 'antallet af scans
+    Const aScany As Integer = 10
+
+    Const pixelSize As Integer = 25
+    Dim pixels(aScanx, aScany) As PictureBox
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Dim n As Integer = 0
+    Sub tegnFelter()
+        For y As Integer = 0 To aScanx
+            For x As Integer = 0 To aScany
+                pixels(x, y) = New PictureBox
+                pixels(x, y).Size = New System.Drawing.Size(pixelSize, pixelSize)
+                pixels(x, y).Location = New System.Drawing.Point(xStart + x * pixelSize, yStart + y * pixelSize)
+
+                Me.Controls.Add(Me.pixels(x, y))
+
+                If n = 0 Then
+                    pixels(x, y).BackColor = Color.Black
+                    n = n + 1
+                Else
+                    pixels(x, y).BackColor = Color.White
+                    n = 0
+                End If
+                'If x Mod 2 = 0 Then
+
+                '    
+                'Else
+                '   
+                'End If
+            Next
+        Next
+
+    End Sub
+
+    Private Sub ButtonTegn_Click(sender As Object, e As EventArgs) Handles ButtonTegn.Click
+        tegnFelter()
+    End Sub
 End Class
