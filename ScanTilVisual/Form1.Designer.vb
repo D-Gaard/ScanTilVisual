72148703f3fd672e2b3e6f4885d5a463fd638e3f
D-Gaard <48207113+D-Gaard@users.noreply.github.com>
2019-04-28 00:06:50 +0200

    Program er done

--- ScanTilVisual/Form1.Designer.vb
+++ ScanTilVisual/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormScanTilVisual
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,30 +20,28 @@ Partial Class FormScanTilVisual
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.ButtonTegn = New System.Windows.Forms.Button()
         Me.TextBoxInput = New System.Windows.Forms.TextBox()
         Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.PictureBoxScaning = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBoxScaning, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonTegn
         '
         Me.ButtonTegn.Location = New System.Drawing.Point(12, 38)
         Me.ButtonTegn.Name = "ButtonTegn"
-        Me.ButtonTegn.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonTegn.Size = New System.Drawing.Size(135, 23)
         Me.ButtonTegn.TabIndex = 0
-        Me.ButtonTegn.Text = "ButtonTegn"
+        Me.ButtonTegn.Text = "Optegn scaning"
         Me.ButtonTegn.UseVisualStyleBackColor = True
         '
         'TextBoxInput
         '
         Me.TextBoxInput.Location = New System.Drawing.Point(12, 67)
         Me.TextBoxInput.Name = "TextBoxInput"
-        Me.TextBoxInput.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxInput.Size = New System.Drawing.Size(135, 20)
         Me.TextBoxInput.TabIndex = 1
         '
         'ContextMenuStrip1
@@ -51,25 +49,15 @@ Partial Class FormScanTilVisual
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
         Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
-        'PictureBoxScaning
-        '
-        Me.PictureBoxScaning.Location = New System.Drawing.Point(688, 12)
-        Me.PictureBoxScaning.Name = "PictureBoxScaning"
-        Me.PictureBoxScaning.Size = New System.Drawing.Size(100, 75)
-        Me.PictureBoxScaning.TabIndex = 3
-        Me.PictureBoxScaning.TabStop = False
-        '
         'FormScanTilVisual
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.PictureBoxScaning)
+        Me.ClientSize = New System.Drawing.Size(507, 583)
         Me.Controls.Add(Me.TextBoxInput)
         Me.Controls.Add(Me.ButtonTegn)
         Me.Name = "FormScanTilVisual"
         Me.Text = "ScanToVisual"
-        CType(Me.PictureBoxScaning, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -78,5 +66,4 @@ Partial Class FormScanTilVisual
     Friend WithEvents ButtonTegn As Button
     Friend WithEvents TextBoxInput As TextBox
     Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
-    Friend WithEvents PictureBoxScaning As PictureBox
 End Class
--- ScanTilVisual/Form1.vb
+++ ScanTilVisual/Form1.vb
@@ -1,54 +1,100 @@
 ﻿Public Class FormScanTilVisual
     '********************************
-    'ScanTilVisual - Skal anvede en tekstfil oprettet af en arduino til
-    '
-    'Krav:
+    'ScanTilVisual - Skal anvede en tekstfil med data til at vise data grafisk
     '
     'Af: Mads Daugaard - April 2019
     '*********************************
 
-    Const xStart As Integer = 100
-    Const yStart As Integer = 100
+    Const xStart As Integer = 100 'forskydning x
+    Const yStart As Integer = 100 'forskydning y
 
-    Const aScanx As Integer = 10 'antallet af scans
-    Const aScany As Integer = 10
+    Const aScanx As Integer = 32 'antallet af scans x
+    Const aScany As Integer = 40 'antallet af scans y
 
-    Const pixelSize As Integer = 25
-    Dim pixels(aScanx, aScany) As PictureBox
+    Const pixelSize As Integer = 10
+    Dim pixels(aScanx - 1, aScany - 1) As PictureBox
+    Dim farve(aScanx - 1, aScany - 1)
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Dim n As Integer = 0
-    Sub tegnFelter()
-        For y As Integer = 0 To aScanx
-            For x As Integer = 0 To aScany
+        For y As Integer = 0 To aScany - 1
+            For x As Integer = 0 To aScanx - 1
                 pixels(x, y) = New PictureBox
                 pixels(x, y).Size = New System.Drawing.Size(pixelSize, pixelSize)
                 pixels(x, y).Location = New System.Drawing.Point(xStart + x * pixelSize, yStart + y * pixelSize)
 
                 Me.Controls.Add(Me.pixels(x, y))
 
-                If n = 0 Then
+
+
+            Next
+        Next
+    End Sub
+
+
+    Sub tegnFelter()
+        For y As Integer = 0 To aScany - 1
+            For x As Integer = 0 To aScanx - 1
+
+                If farve(x, y) = 1 Then
                     pixels(x, y).BackColor = Color.Black
-                    n = n + 1
+
                 Else
                     pixels(x, y).BackColor = Color.White
-                    n = 0
-                End If
-                'If x Mod 2 = 0 Then
 
-                '    
-                'Else
-                '   
-                'End If
+                End If
+
             Next
         Next
 
     End Sub
 
+    Sub gemFarve()
+        Dim scan As String
+        Dim sti As String
+
+        'åbner fil dialog, så der kan vælges en sti
+        Dim dialog As New FolderBrowserDialog()
+        dialog.RootFolder = Environment.SpecialFolder.Desktop
+        dialog.SelectedPath = "C:\" 'drev
+        dialog.Description = "Vælg filen som skal vises" 'forklaring
+        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            'Der er valgt en sti til mappen og den gemmes
+            sti = dialog.SelectedPath
+        End If
+
+        scan = My.Computer.FileSystem.ReadAllText(sti & "\" & TextBoxInput.Text & ".txt")
+        MsgBox(scan)
+        scan = scan.Replace(" ", "")
+        MsgBox(scan)
+        scan = scan.Replace(vbLf, "").Replace(vbCr, "") 'fjerner mellemrum og linje skift
+        MsgBox(scan)
+
+
+
+        For y As Integer = 1 To aScany
+            For x As Integer = 1 To aScanx
+                farve(x - 1, y - 1) = Mid(scan, (x + (y - 1) * aScanx), 1)
+            Next
+        Next
+    End Sub
+
     Private Sub ButtonTegn_Click(sender As Object, e As EventArgs) Handles ButtonTegn.Click
-        tegnFelter()
+        Try
+            gemFarve()
+
+            Dim str As String = ""
+            For y As Integer = 0 To aScany - 1
+                For x As Integer = 0 To aScanx - 1
+                    str = str + farve(x, y)
+                Next
+                str = str & vbNewLine
+            Next
+            MsgBox(str)
+
+            tegnFelter()
+        Catch ex As Exception
+            MsgBox("Filen kunne ikke findes")
+        End Try
+
     End Sub
 End Class
