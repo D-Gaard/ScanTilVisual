97d844e703c57eee5b77580246ed27728b82acab
D-Gaard <48207113+D-Gaard@users.noreply.github.com>
2019-04-28 21:04:12 +0200

    Alt fungerer og kommentare er tilføjet

--- ScanTilVisual/Form1.vb
+++ ScanTilVisual/Form1.vb
@@ -12,19 +12,21 @@
     Const aScany As Integer = 40 'antallet af scans y
 
     Const pixelSize As Integer = 10
-    Dim pixels(aScanx - 1, aScany - 1) As PictureBox
-    Dim farve(aScanx - 1, aScany - 1)
+    Dim pixels(aScanx - 1, aScany - 1) As PictureBox 'array til visning af scan
+    Dim farve(aScanx - 1, aScany - 1)  'array til at holde farve strengen
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'opretter alle picturboxes ved load
         For y As Integer = 0 To aScany - 1
+            'lopper genne arayets y
             For x As Integer = 0 To aScanx - 1
+                'looper gennem arrayets x
+
+                'Opretter en ny picturebox og giver den dens atributter 
                 pixels(x, y) = New PictureBox
                 pixels(x, y).Size = New System.Drawing.Size(pixelSize, pixelSize)
                 pixels(x, y).Location = New System.Drawing.Point(xStart + x * pixelSize, yStart + y * pixelSize)
-
                 Me.Controls.Add(Me.pixels(x, y))
-
-
 
             Next
         Next
@@ -32,13 +34,19 @@
 
 
     Sub tegnFelter()
+        'farver pictureboxesne
         For y As Integer = 0 To aScany - 1
+            'looper y
             For x As Integer = 0 To aScanx - 1
+                'looper x
 
+                'undersøger om farve arrayet indeholder 1 eller 0
                 If farve(x, y) = 1 Then
+                    '1 -> stort
                     pixels(x, y).BackColor = Color.Black
 
                 Else
+                    '0-> hvid
                     pixels(x, y).BackColor = Color.White
 
                 End If
@@ -62,26 +70,29 @@
             sti = dialog.SelectedPath
         End If
 
-        scan = My.Computer.FileSystem.ReadAllText(sti & "\" & TextBoxInput.Text & ".txt")
-        MsgBox(scan)
-        scan = scan.Replace(" ", "")
-        MsgBox(scan)
+        scan = My.Computer.FileSystem.ReadAllText(sti & "\" & TextBoxInput.Text & ".txt") 'læser filen ud fra sti og tekstboks input
+        scan = scan.Replace(" ", "") 'fjerner mellemrum
         scan = scan.Replace(vbLf, "").Replace(vbCr, "") 'fjerner mellemrum og linje skift
-        MsgBox(scan)
 
-
-
+        'indlæser farve
         For y As Integer = 1 To aScany
+            'looper y
             For x As Integer = 1 To aScanx
-                farve(x - 1, y - 1) = Mid(scan, (x + (y - 1) * aScanx), 1)
+                'looper x
+                farve(x - 1, y - 1) = Mid(scan, (x + (y - 1) * aScanx), 1) 'udregner positionen fra teksten og inputter den i farve arrayet
             Next
         Next
     End Sub
 
     Private Sub ButtonTegn_Click(sender As Object, e As EventArgs) Handles ButtonTegn.Click
-        Try
-            gemFarve()
+        'når der klikkes på knappen
 
+        Try
+            'forsøger at indlæse fil og vise den
+
+            gemFarve() 'læser fil og farve
+
+            'viser hvordan strengen ser ud
             Dim str As String = ""
             For y As Integer = 0 To aScany - 1
                 For x As Integer = 0 To aScanx - 1
@@ -91,8 +102,9 @@
             Next
             MsgBox(str)
 
-            tegnFelter()
+            tegnFelter() 'optegner de korrekte farve
         Catch ex As Exception
+            'der blev valgt en ulæselig fil og det fortælles til brugeren
             MsgBox("Filen kunne ikke findes")
         End Try
 
